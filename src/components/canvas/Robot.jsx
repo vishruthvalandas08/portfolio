1ba4c542e77ece74ba5e2f9a287f8a973a82e2aa
vishruthvalandas08 <vishruth.valandasus@gmail.com>
2025-05-22 20:38:28 -0500

    updated robot.jsx3

--- src/components/canvas/Robot.jsx
+++ src/components/canvas/Robot.jsx
@@ -3,11 +3,10 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 import "../../index.css";
-import * as THREE from 'three';
 
 const getSettings = (width, zoomedIn) => {
   if (width <= 500) {
-    return { position: [-1, -2, -3], scale: 1.5 };
+    return { position: [-5, -2, -1.5], scale: 1.5 };
   } else if (width <= 768) {
     return { position: [0, -1.5, -1], scale: 2.2 };
   } else if (width <= 1024) {
@@ -77,23 +76,15 @@ const RobotCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [0, 2, 10], fov: 35 }}
       gl={{ preserveDrawingBuffer: true }}
-      style={{
-        touchAction: 'pan-y ',
-    overscrollBehavior: 'auto',
-      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
-          minPolarAngle={Math.PI / 2.2}    // vertical down limit
+          minPolarAngle={Math.PI / 10}    // vertical down limit
           maxPolarAngle={Math.PI / 2.2}   // vertical up limit
           minAzimuthAngle={-0.52}         // ~-30° horizontal
           maxAzimuthAngle={0.52}          // ~+30° horizontal
           autoRotate={false}            // disable auto-spin
-          touches={{
-            ONE: THREE.TOUCH.PAN,
-            TWO: THREE.TOUCH.ROTATE,
-          }}
         />
         <Robot width={width} />
       </Suspense>
@@ -103,3 +94,82 @@ const RobotCanvas = () => {
 };
 
 export default RobotCanvas;
+
+// import React, { Suspense, useRef, useEffect, useState } from "react";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+// import CanvasLoader from "../Loader";
+// import "../../index.css";
+
+// const Robot = ({ isMobile }) => {
+//   const computer = useGLTF("./future/scene.gltf");
+
+//   return (
+//     <mesh>
+//       <hemisphereLight intensity={0.15} groundColor='black' />
+//       <spotLight
+//         position={[-20, 50, 10]}
+//         angle={0.12}
+//         penumbra={1}
+//         intensity={1}
+//         castShadow
+//         shadow-mapSize={1024}
+//       />
+//       <pointLight intensity={1} />
+//       <primitive
+//         object={computer.scene}
+//         scale={isMobile ? 0.7 : 0.75}
+//         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   );
+// };
+
+// const RobotCanvas = () => {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     // Add a listener for changes to the screen size
+//     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+//     // Set the initial value of the `isMobile` state variable
+//     setIsMobile(mediaQuery.matches);
+
+//     // Define a callback function to handle changes to the media query
+//     const handleMediaQueryChange = (event) => {
+//       setIsMobile(event.matches);
+//     };
+
+//     // Add the callback function as a listener for changes to the media query
+//     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+//     // Remove the listener when the component is unmounted
+//     return () => {
+//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+//     };
+//   }, []);
+
+//   return (
+//     <Canvas
+//       frameloop='demand'
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+//         <Robot isMobile={isMobile} />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default RobotCanvas;
