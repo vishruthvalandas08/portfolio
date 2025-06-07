ef69af8f74caee2febe3b979dc3788a1d7a739d9
vishruthvalandas08 <vishruth.valandasus@gmail.com>
2025-06-06 23:22:33 -0500

    1st commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
 
-export default defineConfig({
-  base: '/portfolio',
-  plugins: [react()],
-});
+const links = [
+  { url: '/', changefreq: 'monthly', priority: 1.0 },
+  { url: '/about', changefreq: 'monthly', priority: 0.8 },
+  { url: '/projects', changefreq: 'monthly', priority: 0.8 },
+];
+
+const sitemap = new SitemapStream({ hostname: 'https://vishruthvalandas08.github.io/portfolio' });
+const writeStream = createWriteStream('./public/sitemap.xml');
+
+streamToPromise(sitemap).then(() => console.log('✅ sitemap.xml generated!'));
+
+sitemap.pipe(writeStream);
+links.forEach(link => sitemap.write(link));
+sitemap.end();
